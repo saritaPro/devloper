b44cbc16d2051f645baaba9e92ac5371ff6af01f
saritaPro <74887580+saritaPro@users.noreply.github.com>
2021-01-07 19:47:43 +0530

    Assignment operators
    c++ program for Assignment Operators.

---
+++ Assignment operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=50, b=70;
+	a+=b;
+	cout<<"Addition of number is:"<<a<<endl;
+	a-=b;
+	cout<<"Substraction of number is:"<<a<<endl;
+	a*=b;
+	cout<<"Multiplication of number is:"<<a<<endl;
+	a/=b;
+	cout<<"Division of number is:"<<a<<endl;
+	return 0;
+}
