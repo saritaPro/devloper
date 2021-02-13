f14e821118dbb71448f507f78ec4aaac34a7b0b9
Sarita Patil <74887580+saritaPro@users.noreply.github.com>
2021-02-13 19:53:22 +0530

    Add files via upload

---
+++ break_01.py
@@ -0,0 +1,19 @@
+a = 10
+while(a <= 20):
+	print "Value of a is: ", a
+	a = a + 1
+	if(a == 16):
+		a = a + 1
+		break
+
+
+#Output
+"""
+Value of a is:  10
+Value of a is:  11
+Value of a is:  12
+Value of a is:  13
+Value of a is:  14
+Value of a is:  15
+
+"""
