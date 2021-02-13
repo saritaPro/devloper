e4082d388cc7fee1744d0e883102a9d2e74d07da
Sarita Patil <74887580+saritaPro@users.noreply.github.com>
2021-02-13 19:53:46 +0530

    Add files via upload

---
+++ continue_01.py
@@ -0,0 +1,23 @@
+a = 10
+while(a <= 20):
+	print "Value of a is: ", a
+	a = a + 1
+	if(a == 16):
+		a = a + 1
+		continue
+
+
+#Output
+"""
+Value of a is:  10
+Value of a is:  11
+Value of a is:  12
+Value of a is:  13
+Value of a is:  14
+Value of a is:  15
+Value of a is:  17
+Value of a is:  18
+Value of a is:  19
+Value of a is:  20
+
+"""
